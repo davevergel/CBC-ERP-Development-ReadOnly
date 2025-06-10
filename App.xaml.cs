1437dc9590ac2f0eb3e28400bdd66d6f2418790f
davevergel <57636877+davevergel@users.noreply.github.com>
2025-06-10 15:18:35 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using Microsoft.Extensions.Configuration;
+using CbcRoastersErp.Services;
+using CbcRoastersErp.Helpers;
+using CbcRoastersErp.Helpers.converters;
+using CbcRoastersErp.Helpers.Converters;
+
+namespace CbcRoastersErp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Load Configuration
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            ConfigHelper.Initialize(config);
+            DatabaseService.Initialize(config);
+
+            // Register global resources
+            Resources.Add("BoolToVisibilityConverter", new BoolToVisibilityConverter { CollapseInsteadOfHidden = true });
+            Resources.Add("IsGreaterThanOneConverter", new IsGreaterThanOneConverter());
+            Resources.Add("IsLessThanTotalPagesConverter", new IsLessThanTotalPagesConverter());
+            Resources.Add("BooleanToVisibilityConverter", new BooleanToVisibilityConverter() );
+            Resources.Add("InverseBooleanToVisibilityConverter", new InverseBooleanToVisibilityConverter());
+            Resources.Add("NullToBoolConverter", new NullToBoolConverter());
+        }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using CbcRoastersErp.Views;
+using CbcRoastersErp.ViewModels;
+
+namespace CbcRoastersErp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DashboardViewModel _dashboardViewModel;
+        private InventoryView _inventoryView;
+        private ProductionView _productionView;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
