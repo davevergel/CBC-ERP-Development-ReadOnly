ee0fd90e846fd348eec2b0749ed41c0f52d680d2
davevergel <57636877+davevergel@users.noreply.github.com>
2025-06-10 15:17:21 -0500

    Add files via upload

---
+++ Views/Administration/UserManagement/AddEditUserView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using CbcRoastersErp.ViewModels;
+
+namespace CbcRoastersErp.Views
+{
+    /// <summary>
+    /// Interaction logic for AddEditUserView.xaml
+    /// </summary>
+    public partial class AddEditUserView : UserControl
+    {
+        public AddEditUserView()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is AddEditUserViewModel vm)
+            {
+                vm.PlainPassword = ((PasswordBox)sender).Password;
+            }
+        }
+    }
+}
---
+++ Views/HR/AddEditPerformanceReviewView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CbcRoastersErp.Views.HR
+{
+    /// <summary>
+    /// Interaction logic for AddEditPerformanceReviewView.xaml
+    /// </summary>
+    public partial class AddEditPerformanceReviewView : UserControl
+    {
+        public AddEditPerformanceReviewView()
+        {
+            InitializeComponent();
+        }
+    }
+}
